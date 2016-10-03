2eec73eba3cc837c8705c882465f1790aacaccd3
Tom Evans <tevans@astro.ex.ac.uk>
2016-10-03 16:26:32 +0100

    Have started adding options for chem=='man', i.e. manual chemistry rather than just equilibrium

--- pyARC/ModelLogLike.py
+++ pyARC/ModelLogLike.py
@@ -9,7 +9,7 @@ This module contains routines for evaluating the log likelihood for some standar
 """
 
 
-def ClearChemEqTransmission( parsarr, keys, ARC ):
+def ClearChemEqIsothermTransmission( parsarr, keys, ARC ):
     """
     Evaluates the log likelihood for a clear atmosphere in chemical equilibrium
     assuming an isothermal PT profile given an observed spectrum for the planet. 
@@ -28,6 +28,9 @@ def ClearChemEqTransmission( parsarr, keys, ARC ):
     """
     
     t1 = time.time()
+    tmpdir = os.path.join( os.getcwd(), 'tmp' )
+    if os.path.isdir( tmpdir )==False:
+        os.makedirs( tmpdir )
 
     datasets = ARC.TransmissionData.keys()
     ndatasets = len( datasets )
@@ -53,6 +56,8 @@ def ClearChemEqTransmission( parsarr, keys, ARC ):
     # If we're in an allowable region of parameter space,
     # proceed to run ATMO:
     if np.isfinite( logp_prior ):
+        # Ensure atmosphere is isothermal:
+        ARC.ATMO.fin = None
         # Install values for the free parameters:
         ARC.ATMO.teff = pars['Teff']
         ARC.ATMO.MdH = pars['MdH']
@@ -60,14 +65,19 @@ def ClearChemEqTransmission( parsarr, keys, ARC ):
 
         # Use tempfile to create input and output files so that there
         # will be no duplication e.g. if running many walkers: 
-        tempfileobj1 = tempfile.NamedTemporaryFile( mode='w+b', delete=False )
-        ARC.ATMO.ftrans_spec = tempfileobj1.name
-        tempfileobj2 = tempfile.NamedTemporaryFile( mode='w+b', delete=False )
-        ARC.ATMO.infile_path = tempfileobj2.name
+        #tempfileobj1 = tempfile.NamedTemporaryFile( mode='w+b', dir=tmpdir, suffix='.ncdf', delete=False )
+        #tempfileobj2 = tempfile.NamedTemporaryFile( mode='w+b', dir=tmpdir, suffix='.in', delete=False )
+        #ARC.ATMO.infile_path = tempfileobj2.name
+        #ARC.ATMO.ftrans_spec = tempfileobj1.name        
+        tempfileobj = tempfile.NamedTemporaryFile( mode='w+b', dir=tmpdir, suffix='.in', delete=False )
+        ARC.ATMO.infile_path = tempfileobj.name
+        ARC.ATMO.ftrans_spec = tempfileobj.name.replace( '.in', '.ncdf' )
 
         # Compute the model transmission spectrum:
         ARC.ATMO.RunATMO()
-        ARC.ATMO.ReadTransmissionModel( ncdf_fpath=tempfileobj1.name )
+        ARC.ATMO.ReadTransmissionModel( ncdf_fpath=ARC.ATMO.ftrans_spec )
+        #ARC.ATMO.ReadTransmissionModel( ncdf_fpath=tempfileobj1.name )
+        #print 'zzzzzzz 2', pars['Teff'], ARC.ATMO.teff, tempfileobj1.name        
         WavMicronModel = ARC.ATMO.TransmissionModel[:,0]
         RpRsModel = ARC.ATMO.TransmissionModel[:,1] - pars['dRpRs']
 
@@ -94,8 +104,11 @@ def ClearChemEqTransmission( parsarr, keys, ARC ):
         ResidsArr = DataArr - ModelArr
         ndat = len( ResidsArr )
         logp_data = logp_mvnormal_whitenoise( ResidsArr, UncArr, ndat )
-        os.remove( tempfileobj1.name )
-        os.remove( tempfileobj2.name )
+        #os.remove( tempfileobj1.name )
+        #os.remove( tempfileobj2.name )
+        os.remove( ARC.ATMO.infile_path )
+        os.remove( ARC.ATMO.ftrans_spec ) # in future, want to save these models at each step
+        
         # TODO = COULD SAVE ALL OF THESE TRANSMISSION SPECTRA
         # FOR PLOTTING AT THE END
         #print '\n\n\n aaaaaaaa', logp_data, logp_prior, 'bbbbbbbbb\n\n\n'
